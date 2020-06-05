8922f60b5c14026f121c46f0f80c6d0191a47769
renata faria <renata.faria.gomes@hotmail.com>
2020-06-04 22:08:01 -0300

    fixed php form

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -19,8 +19,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'renata.faria.gomes@hotmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+mail($toEmail, $subject, $themessage, $headers);
+//mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
