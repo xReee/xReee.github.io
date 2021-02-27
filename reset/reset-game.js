bca60d26704f16cda17d65bd48b124789097a14e
renata faria <renata.faria.gomes@hotmail.com>
2021-02-27 17:02:43 -0300

    added reset game files

--- reset/reset-game.js
+++ reset/reset-game.js
@@ -1,58 +1,52 @@
-var count1c = 0;
-var count2c = 0;
-var count3c = 0;
-var count4c = 0;
-var count5c = 0;
-var count6c = 0;
+var password = [];
+var userPassword = [0, 0, 0, 0];
 var cores = [
     "rgb(255, 0, 0)",
     "rgb(0, 0, 255)",
     "rgb(255, 255, 0)",
-     "rgb(128, 0, 128)",
-      "rgb(0, 255, 0)",
-       "rgb(255, 165, 0)",
-        "rgb(255, 192, 203)",
-         "rgb(128, 128, 128)"
-        ];
+    "rgb(128, 0, 128)",
+    "rgb(0, 255, 0)",
+    "rgb(255, 165, 0)",
+  ];
+var answer = [];
 
-var mudaCor = function(elem){
-    switch ($(elem).attr("id")) {
-      case "n1":
-        $(elem).css("background-color", cores[count1c]);
-        count1c ++;
-        if (count1c >= 8) count1c = 0;
-        break;
-      case "n2":
-          $(elem).css("background-color", cores[count2c]);
-          count2c ++;
-          if (count2c >= 8) count2c = 0;
-          break;
-      case "n3":
-            $(elem).css("background-color", cores[count3c]);
-            count3c ++;
-            if (count3c >= 8) count3c = 0;
-            break;
-      case "n4":
-            $(elem).css("background-color", cores[count4c]);
-            count4c ++;
-            if (count4c >= 8) count4c = 0;
-            break;
-    case "n5":
-        $(elem).css("background-color", cores[count5c]);
-        count5c ++;
-        if (count5c >= 8) count5c = 0;
-        break;
-    case "n6":
-        $(elem).css("background-color", cores[count6c]);
-        count6c ++;
-        if (count6c >= 8) count6c = 0;
-        break;
-    }
+var changeTool = function(elem) {
+  var id = parseInt($(elem).attr("id").replace('n','')) - 1;
+  $(elem).css("background-color", cores[userPassword[id]]);
+  userPassword[id] ++;
+  if (userPassword[id] >= 6) userPassword[id] = 0;
+}
+
+var generatePassword = function() {
+  var newPassword = [];
+  for (var i = 0; i <= 3; i++) {
+    newPassword[i] = Math.floor((Math.random() * 6));
   }
+  return newPassword;
+}
 
+var checkPassword = function() {
+  var rightAnwsers = 0;
+  var rightColorWrongPositions = 0;
+  console.log(password);
+  console.log(userPassword);
+  for(var i = 0; i <= 3; i++) {
+    if (password[i] == userPassword[i]) {
+      rightAnwsers++;
+    } else if (password.includes(userPassword[i])) {
+      rightColorWrongPositions++;
+    } 
+  }
+  console.log(rightAnwsers);
+  console.log(rightColorWrongPositions);
+}
 
 $(document).ready(function() {
-    $(".border-8px-hot-toddy").click(function(){
-        mudaCor(this);
-      });
+  password = generatePassword();
+
+  $(".slot").click(function(){
+    changeTool(this);
+  });
+
+  $(".testButton").click(checkPassword);
 });
