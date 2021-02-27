bdb325e8df18d30c64d45dd9962c2db2e4d017e6
renata faria <renata.faria.gomes@hotmail.com>
2021-02-27 17:48:29 -0300

    Added logic

--- reset/reset-game.js
+++ reset/reset-game.js
@@ -20,8 +20,15 @@ var changeTool = function(elem) {
 var generatePassword = function() {
   var newPassword = [];
   for (var i = 0; i <= 3; i++) {
-    newPassword[i] = Math.floor((Math.random() * 6));
+    var number = Math.floor((Math.random() * 6));
+    if (newPassword.includes(number)) {
+      i--;
+    } else {
+      newPassword[i] = number;
+    }
   }
+
+  console.log(newPassword);
   return newPassword;
 }
 
@@ -37,8 +44,18 @@ var checkPassword = function() {
       rightColorWrongPositions++;
     } 
   }
+  speak(rightAnwsers, rightColorWrongPositions);
   console.log(rightAnwsers);
   console.log(rightColorWrongPositions);
+}
+
+function speak(rightAnwsers, rightColorWrongPositions){
+  var rightAnwsersLabel = " arma na posição correta";
+  var rightColorWrongPositionsLabel = " arma correta está no slot errado";
+  if (rightAnwsers != 1) rightAnwsersLabel = " armas nas posições corretas";
+  if (rightColorWrongPositions != 1) rightColorWrongPositionsLabel = " armas corretas estão no slot errado!";
+  $(".speach").text("Hmmm.. você acertou " + rightAnwsers + rightAnwsersLabel + "! Tirando isso, " + 
+                      rightColorWrongPositions + rightColorWrongPositionsLabel);
 }
 
 $(document).ready(function() {
